913ac02a7c7778069c435e967dcc9f09156c2806
andaihn <72494054+andaihn-ai@users.noreply.github.com>
2021-03-15 09:31:36 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "array.h"
+
+
+int main()
+{
+    // 내부적으로 int 형을 저장하는 array 
+    // <> 안의 타입을 받아 타입에 타겟팅되는 소스코드 생성
+
+    int nums[]={1, 2, 3, 4, 5 };                    // int형 nums 배열 생성
+    Array<int> arr1(nums, 5);                       // int 형 array코드
+
+    for(int i = 0; i < arr1.size(); ++i)
+    {
+        std::cout << arr1[i] << std::endl;
+    }
+
+    double nums2[]={1.1, 2.2, 3.3, 4.4, 5.5 };
+    Array<double> arr2(nums2, 5);                   // double 형 array코드
+
+    for(int i = 0; i < arr2.size(); ++i)
+    {
+        std::cout << arr2[i] << std::endl;
+    }
+
+    return 0;
+}
